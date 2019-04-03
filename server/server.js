5d468ccc253407e299df2c70019173804de38f14
lilvina <davinaleo2007@yahoo.com>
2019-04-03 15:07:14 -0700

    finished auth and user router but fixing some bugs

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ const cors = require('cors')
 
 const server = express()
 
+const authRouter = require('../routes/auth-router.js')
+const userRouter = require('../routes/users-router.js')
+
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send("I am alive and working!")
